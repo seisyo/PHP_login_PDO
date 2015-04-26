7156c0c7be53b60ba7c9d287e6364d4c0fd7e1f2
seisyo <seisyo1234@gmail.com>
2015-04-26 23:33:31 +0800

    SQL_class change the method pdo_query to the prepare+execute, but it always return the false, and the null value.

--- SQL_class.php
+++ SQL_class.php
@@ -1,33 +1,48 @@
 <?php
 class SQL_class{
 	private  $link;
-	private  $result;
-	public function __construct(){
+	
+	public function __construct($db_host = "127.0.0.1", $db_name, $db_user, $db_password){
 		//匯入連線資訊
-		include("connect_info.php");
+		$dsn = "mysql:dbname={$db_name};host={$db_host};charset=UTF8";
 		//連線到資料庫
 		try{
-			$this->link = new PDO($db_server, $db_user, $db_password);
+			$this->link = new PDO($dsn, $db_user, $db_password);
 		}catch(PDOException $e){
 			printf("DatabaseError: %s", $e->getMessage());
 			}
-		echo "Connect successfully!";
+		echo "Connect successfully!"."<br>";
 	}
-	public function selection(){
+	
+	public function selection($table_name, $columns = []){
 		//查詢資料並輸出
 		try{
-			$this->result = $this->link->query("select `md5_password` from `login`;");
+			if(empty($columns)){
+				$columns = '*';
+			}elseif(is_string($columns)){
+				return 'Please input array!';
+			}else{
+				$columns = implode(",",$columns);
+			}
+			//echo $columns."<br>";
+			//echo $table_name."<br>";
+
+			$result = $this->link->prepare("select :columns from :table;");
+			//$result->bindParam(":columns", $columns);
+			//$result->bindParam(":table_name", $table_name);
+			$result->execute(array(':columns'=>$columns, ':table'=>$table_name));
+			//$result->execute();
+
 		}catch(PDOException $e){
 			printf("Database Connect Error: %s", $e->getMessage());
 			}
-		
-		
-		var_dump($this->result->fetchAll(PDO::FETCH_ASSOC));
+		var_dump($result->fetchAll(PDO::FETCH_NUM));
 	}
+	
 	public function insertion(){
 		//插入新登入資料
 		try{
-			$this->result = $this->link->exec("insert into `login` (`account`,`md5_password`) values ('abc','202cb962ac59075b964b07152d234b70')");
+			$result = $this->link->exec("insert into `login` (`account`,`md5_password`) values ('abc','202cb962ac59075b964b07152d234b70')");
 		}catch(PDOException $e){
 			printf("Database Connect Error: %s", $e->getMessage());
 			}
@@ -37,6 +52,6 @@ class SQL_class{
 		echo "Bye~!";
 	}
 }
-$o = new SQL_class();
-$o -> selection();
-$o -> insertion();
+$o = new SQL_class('127.0.0.1','rakuda_seisyo', 'rakuda', 'QzcE2BXsyp6nU3MD');
+$o->selection('`login`',['`account`','`md5_password`']);
+// $o -> insertion();
