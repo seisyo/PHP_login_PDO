de7d22bd4d7a0e0d74623fefa53e4c6c67ae5a29
seisyo <seisyo1234@gmail.com>
2015-04-28 00:51:02 +0800

    The standard insert&select function caan run, but it isn't perfect.

--- SQL_class.php
+++ SQL_class.php
@@ -8,50 +8,69 @@ class SQL_class{
 		//連線到資料庫
 		try{
 			$this->link = new PDO($dsn, $db_user, $db_password);
+			//$this->link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 		}catch(PDOException $e){
 			printf("DatabaseError: %s", $e->getMessage());
 			}
 		echo "Connect successfully!"."<br>";
 	}
 	
-	public function selection($table_name, $columns = []){
+	// public function implodecolumns($cols = []){
+	// 	if(empty($cols)){
+	// 		$cols= '*';
+	// 	}elseif(is_string($cols)){
+	// 		return 'Please input array!';
+	// 		break;
+	// 	}else{
+	// 		$cols = implode(",",$cols);
+	// 	}
+	// 	return $cols;
+	// }
+
+	public function selection($table_name, $columns = [], $wherecon = 1, $whereval = 1){
 		//查詢資料並輸出
 		try{
-			if(empty($columns)){
-				$columns = '*';
+			if(empty($columns)){//先將columns裡的欄位連接成字串
+				$columns= '*';
 			}elseif(is_string($columns)){
 				return 'Please input array!';
 			}else{
 				$columns = implode(",",$columns);
 			}
-			//echo $columns."<br>";
-			//echo $table_name."<br>";
-
-			$result = $this->link->prepare("select :columns from :table;");
-			//$result->bindParam(":columns", $columns);
-			//$result->bindParam(":table_name", $table_name);
-			$result->execute(array(':columns'=>$columns, ':table'=>$table_name));
-			//$result->execute();
-
+			$sql = "select ".$columns." from ".$table_name." where".$wherecon."= :whereval;";//先將變數以外的地方串接起來
+			$sth = $this->link->prepare($sql);
+			$sth->execute(array(':whereval'=>$whereval));//再prepare變數執行
 		}catch(PDOException $e){
 			printf("Database Connect Error: %s", $e->getMessage());
 			}
-		var_dump($result->fetchAll(PDO::FETCH_NUM));
+		$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+		//$result->fetchAll(PDO::FETCH_NUM);//將結果fetch出來
+		var_dump($result);
 	}
 	
-	public function insertion(){
-		//插入新登入資料
+	public function insertion($table_name, $columns = [], $account, $content){
+		//插入新資料
 		try{
-			$result = $this->link->exec("insert into `login` (`account`,`md5_password`) values ('abc','202cb962ac59075b964b07152d234b70')");
+			if(empty($columns)){//先將columns裡的欄位連接成字串
+				$columns= '*';
+			}elseif(is_string($columns)){
+				return 'Please input array!';
+			}else{
+				$columns = implode(",",$columns);
+			}
+			$sql = "insert into ".$table_name." ( ".$columns." ) "." values (:account,:content);";
+			$sth = $this->link->prepare($sql);
+			$sth->execute(array(':account'=>$account, ':content'=>md5($content)));//再prepare變數執行
 		}catch(PDOException $e){
 			printf("Database Connect Error: %s", $e->getMessage());
 			}
 	}
+	
 	public function __destruct(){
 		$this->link = null;
-		echo "Bye~!";
+		echo "<br>"."Bye~!";
 	}
 }
 $o = new SQL_class('127.0.0.1','rakuda_seisyo', 'rakuda', 'QzcE2BXsyp6nU3MD');
-$o->selection('`login`',['`account`','`md5_password`']);
-// $o -> insertion();
+$o->selection('`login`',['`account`','`md5_password`'],'`account`','abc');
+$o -> insertion('`login`',['`account`','`md5_password`'],'seisyo','12345678');
